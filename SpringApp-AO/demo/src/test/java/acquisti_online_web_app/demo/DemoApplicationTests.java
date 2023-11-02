d30e20107cfec74d07874cff371c6eb6a6f3a25b
Gabriele-Sucato <127972268+Gabriele-Sucato@users.noreply.github.com>
2023-11-02 10:31:42 +0100

    SpringBoot project init

---
+++ SpringApp-AO/demo/src/test/java/acquisti_online_web_app/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package acquisti_online_web_app.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
