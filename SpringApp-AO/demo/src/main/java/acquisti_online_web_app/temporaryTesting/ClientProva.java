6e2c8ea06dec66fac608b61bc31a935c3dfdbbc7
Emanuele Simula <simulaemanuele@gmail.com>
2023-11-02 17:35:43 +0100

    test: implemented class for testing porpuse on git flow and working directory

---
+++ SpringApp-AO/demo/src/main/java/acquisti_online_web_app/temporaryTesting/ClientProva.java
@@ -0,0 +1,14 @@
+package acquisti_online_web_app;
+
+public class ClientProva {
+    public String name;
+    public String lastName;
+    public Integer age;
+
+    public ClientSample(String name, String lastName, Integer age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+}
