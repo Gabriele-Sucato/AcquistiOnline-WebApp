e6e3e17c84f63a9831e5bd183f56a532aa1b2b04
Anteo1599 <cananzimattia@gmail.com>
2023-11-06 17:14:20 +0100

    Revert "feat: created Article class and interface ArticleRepository and going to write ArticleRepoClass."
    This reverts commit 56623ae4707c1587b9fc052d4fd154ec75d448cb.

--- src/main/java/repository/ArticleRepository.java
+++ src/main/java/repository/ArticleRepository.java
@@ -1,21 +1,5 @@
 package main.java.repository;
 
-import java.util.List;
-
-import main.java.model.Article;
-
-public interface ArticleRepository
-
-{
-
-    Article findArticleByName(String name);
-
-    List<Article> findAllArticles();
-
-    void saveArticle(Article article);
-
-    void updateArticle(Article article);
-
-    void deleteArticle(String name);
+public class ArticleRepository {
 
 }
