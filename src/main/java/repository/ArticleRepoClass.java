66c69361a0764879f6d0831d5fece98e5d0e9524
Anteo1599 <cananzimattia@gmail.com>
2023-11-06 17:18:58 +0100

    feat: created Article class and interface ArticleRepository and going to write ArticleRepoClass.

---
+++ src/main/java/repository/ArticleRepoClass.java
@@ -0,0 +1,23 @@
+package main.java.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.model.Article;
+
+public class ArticleRepoClass implements ArticleRepository{
+    
+    private List<Article> articles = new ArrayList<>();
+
+
+    @Override
+    public Article findArticleByName(String name){
+        for (Article article : articles) {
+            if(article.getNameArticle() == name ){
+                return article;
+            }
+        }
+        return
+    }
+
+}
