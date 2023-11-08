df8c896c1c088f21d02dc697f8f79253bf371163
Emanuele Simula <simulaemanuele@gmail.com>
2023-11-08 17:59:07 +0100

    softfix: fixed syntax error in class Cart.java. Now everything is fine!

--- src/main/java/model/Cart.java
+++ src/main/java/model/Cart.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
-    private List<Article> articles= new ArrayList<>();
+    private List<Article> articles = new ArrayList<>();
     private String nameCl;
     private String lastnameCl;
     private String paymentMethod;
@@ -13,7 +13,7 @@ public class Cart {
     private double priceSingle;
     private double priceTot;
 
-public Cart(String nameCl, String lastnameCl, String paymentMethod, String nameArticle, int quantity,
+    public Cart(String nameCl, String lastnameCl, String paymentMethod, String nameArticle, int quantity,
             double priceSingle, double priceTot) {
         this.nameCl = nameCl;
         this.lastnameCl = lastnameCl;
@@ -25,77 +25,82 @@ public Cart(String nameCl, String lastnameCl, String paymentMethod, String nameA
     }
 
     public Cart() {
-}
+    }
 
     public String getNameCl() {
         return nameCl;
     }
+
     public void setNameCl(String nameCl) {
         this.nameCl = nameCl;
     }
 
-
     public String getLastnameCl() {
         return lastnameCl;
     }
+
     public void setLastnameCl(String lastnameCl) {
         this.lastnameCl = lastnameCl;
     }
-    
 
     public String getPaymentMethod() {
         return paymentMethod;
     }
+
     public void setPaymentMethod(String paymentMethod) {
         this.paymentMethod = paymentMethod;
     }
 
-
     public String getNameArticle() {
         return nameArticle;
     }
+
     public void setNameArticle(String nameArticle) {
         this.nameArticle = nameArticle;
     }
 
-
     public int getQuantity() {
         return quantity;
     }
+
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
-
     public double getPriceSingle() {
         return priceSingle;
     }
+
     public void setPriceSingle(double priceSingle) {
         this.priceSingle = priceSingle;
     }
 
-
     public double getPriceTot() {
         return priceTot;
     }
+
     public void setPriceTot(double priceTot) {
         this.priceTot = priceTot;
     }
-    
-    public void addArticle(Article article, int quantity) {
-for (Article exArticle : articles) {
-    if (exArticle.getNameArticle().equals(article.getNameArticle())) { {
 
+    public void addArticle(Article article, int quantity) {
+        for (Article exArticle : articles) {
+            if (exArticle.getNameArticle().equals(article.getNameArticle())) {
+                {
+
+                }
+            }
+        }
     }
-}
-    }
+
     @Override
     public String toString() {
         return "Cart [nameCl=" + nameCl + ", lastnameCl=" + lastnameCl + ", paymentMethod=" + paymentMethod
                 + ", nameArticle=" + nameArticle + ", quantity=" + quantity + ", priceSingle=" + priceSingle
                 + ", priceTot=" + priceTot + "]";
     }
-public static void main(String[] args) {
-   
-}
+
+    public static void main(String[] args) {
+
+    }
 }
