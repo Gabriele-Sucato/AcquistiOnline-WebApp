567fcd7e1726710bb94d24ab1bb30f5cec6ab5ca
Emanuele Simula <simulaemanuele@gmail.com>
2023-11-09 22:32:58 +0100

    refact: moved and separate in repository package in 2 subpackages repoClasses and interfaces

--- src/main/java/repository/interfaces/ArticleRepository.java
+++ src/main/java/repository/interfaces/ArticleRepository.java
@@ -1,14 +1,13 @@
 package main.java.repository;
 
-import java.util.List;
-
 import main.java.model.Article;
 
-public interface ArticleRepository{
- 
+public interface ArticleRepository {
+
         Article findArticleByCode(String code);
+
         void saveArticle(Article article);
+
         void updateArticle(Article article);
-    
 
 }
