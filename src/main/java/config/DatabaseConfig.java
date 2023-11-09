1bf8fc60f8a9b433bc2111e8fbeba77889a8f10d
Emanuele Simula <simulaemanuele@gmail.com>
2023-11-09 13:44:31 +0100

    feat: created folder config with DatabaseConfig class in it that can handle the connection to DB. Created file config.properties in resources to abstract the props setting inside the project

---
+++ src/main/java/config/DatabaseConfig.java
@@ -0,0 +1,41 @@
+package main.java.config;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConfig {
+
+    private static final String PROPERTIES_FILE = "config.properties";
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream input = DatabaseConfig.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
+            if (input != null) {
+                properties.load(input);
+            } else {
+                System.err.println("Configuration file '" + PROPERTIES_FILE + "' not found!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getJdbcUrl() {
+        return properties.getProperty("jdbc.url");
+    }
+
+    public static String getJdbcUser() {
+        return properties.getProperty("jdbc.user");
+    }
+
+    public static String getJdbcPassword() {
+        return properties.getProperty("jdbc.password");
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(getJdbcUrl(), getJdbcUser(), getJdbcPassword());
+    }
+}
